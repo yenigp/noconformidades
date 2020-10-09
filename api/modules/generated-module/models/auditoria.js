d4ca7b12dfca95027defe32c4fe8f683fb5f1647
Yeni <hav_yeni@hotmail.com>
2020-10-09 08:59:32 -0400

    Actualizado modelo auditoria
    Eliminado esquema e id. Actualizado nombre de la tabla y noconformidad

--- api/modules/generated-module/models/auditoria.js
+++ api/modules/generated-module/models/auditoria.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
     const Auditoria = global.app.orm.sequelize.define('Auditoria',
         lodash.extend({}, global.app.orm.mixins.attributes, {
-          "id": {
+          "NoConformidadId": {
               "type": global.app.orm.Sequelize.INTEGER,
               "references": {
                   "model": "NoConformidad",
@@ -12,26 +12,38 @@ exports.loadModel = function loadModel() {
               },
               "onUpdate": "cascade",
               "onDelete": "cascade",
-              "allowNull": false,
-              "primaryKey": true
+              "allowNull": false
+
           },
             "observacion": {
                 "type": global.app.orm.Sequelize.STRING,
                 "allowNull": false
 
             },
+            "CreatorId": {
+                "type": global.app.orm.Sequelize.INTEGER,
+                "references": {
+                    "model": "Usuario",
+                    "key": "id"
+                },
+                "onUpdate": "cascade",
+                "onDelete": "cascade"
+    
+            },
 
         }), {
             comment: 'A example model.',
             freezeTableName: true,
-            tableName: 'auditoria',
-            schema: 'noconformidades',
+            tableName: 'Auditoria',
             hooks: {
 
             }
         });
         Auditoria.associate = function() {
             var models = global.app.orm.sequelize.models;
+            models.Auditoria.belongsTo(models.Usuario, {
+                as: 'Creator'
+            });  
             models.Auditoria.belongsTo(models.NoConformidad, {
                 as: 'NoConformidad'
             });
