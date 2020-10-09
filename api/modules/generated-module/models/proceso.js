cb7c8e397f0b14d96c143d836f241900905c09ae
Yeni <hav_yeni@hotmail.com>
2020-10-09 08:36:57 -0400

    Actualizado modelo proceso

--- api/modules/generated-module/models/proceso.js
+++ api/modules/generated-module/models/proceso.js
@@ -4,20 +4,20 @@ const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
     const Proceso = global.app.orm.sequelize.define('Proceso',
         lodash.extend({}, global.app.orm.mixins.attributes, {
-          "id": {
-            "type": global.app.orm.Sequelize.INTEGER,
+          "codigo": {
+            "type": global.app.orm.Sequelize.STRING,
             "allowNull": false,
-            "autoIncrement": true,
-            "primaryKey": true
+            "validate":{
+              "max": 2
+            }
           },
             "nombre": {
                 "type": global.app.orm.Sequelize.STRING,
                 "allowNull": false,
                 "validate":{
-                  "is": {
-                    "args": /^([A-Z]{1}[a-zñáéíóú]+[\s]*)+$/i,
-                    "msg": "Sólo se aceptan letras"
-                  },
+                  /*"isAlpha": {
+                    "msg": "El nombre de un proceso solo puede contener letras"
+                  },*/
                   "len":{
                     "args": [3,50],
                     "msg": "Mínimo 3 y máximo 50 carácteres"
@@ -25,19 +25,53 @@ exports.loadModel = function loadModel() {
                 }
             },
             "tipo": {
-                "type": global.app.orm.Sequelize.STRING,
-                "allowNull": true
+                "type": global.app.orm.Sequelize.ENUM,
+                "values": ["realización", "estratégico", "de apoyo"],
+                "allowNull": false
 
             },
+            "jefeproceso": {
+              "type": global.app.orm.Sequelize.INTEGER,
+              "allowNull": false
+      
+            },
+            "CreatorId": {
+              "type": global.app.orm.Sequelize.INTEGER,
+              "references": {
+                  "model": "Usuario",
+                  "key": "id"
+              },
+              "onUpdate": "cascade",
+              "onDelete": "cascade"
+    
+          },
 
         }), {
             comment: 'A example model.',
             freezeTableName: true,
-            tableName: 'proceso',
-            schema: 'noconformidades',
+            tableName: 'Proceso',
             hooks: {
 
             }
         });
+        Proceso.associate = function() {
+          var models = global.app.orm.sequelize.models;
+          models.Proceso.belongsTo(models.Usuario, {
+              as: 'Creator'
+          });  
+          models.Proceso.belongsTo(models.Usuario, {
+            foreignKey: 'jefeproceso',
+            //constraints: false
+          }); 
+          models.Proceso.hasMany(models.Indicadores, {
+              as: "Indicadores",
+              foreignKey: "IndicadorId"
+
+          });
+          models.Proceso.hasMany(models.NoConformidad, {
+            as: "NoConformidad",
+            foreignKey: "ProcesoId"
+        });
+        }  
 
 };
