3ad8edbd62c3397ec82c4a242cfd7c8f57fa3860
Yeni <hav_yeni@hotmail.com>
2020-10-09 09:51:54 -0400

    Actualizado modelo mercadopais
    Eliminado id, esquema. Actualizado mercado, pais y nombre de la tabla.

--- api/modules/generated-module/models/mercadopais.js
+++ api/modules/generated-module/models/mercadopais.js
@@ -2,41 +2,27 @@
 var lodash = require('lodash');
 const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
-    const MercadoPais = global.app.orm.sequelize.define('MercadoPais',
-        lodash.extend({}, global.app.orm.mixins.attributes, {
-          "PaisID": {
-              "type": global.app.orm.Sequelize.INTEGER,
-              "references": {
-                  "model": "Pais",
-                  "key": "idpais"
-              },
-              "onUpdate": "cascade",
-              "onDelete": "cascade",
-              "allowNull": false
-          },
-          "MercadoID": {
-              "type": global.app.orm.Sequelize.INTEGER,
-              "references": {
-                  "model": "Mercado",
-                  "key": "idmercado"
-              },
-              "onUpdate": "cascade",
-              "onDelete": "cascade",
-              "allowNull": false
-          },
-            "activo": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "defaultValue": 1
-
-            },
-
-        }), {
+  const MercadoPais = global.app.orm.sequelize.define('MercadoPais',
+    lodash.extend({}, global.app.orm.mixins.attributes, {
+      "MercadoId": {
+        "type": global.app.orm.Sequelize.INTEGER
+      },
+      "PaisId": {
+        "type": global.app.orm.Sequelize.INTEGER
+      },
+      "activo": {
+        "type": global.app.orm.Sequelize.INTEGER
+      }
+    }), {
+      
             comment: 'A example model.',
+            timestamps: false,
+            paranoid: true,
             freezeTableName: true,
-            tableName: 'e_mercadopais',
-            schema: 'noconformidades',
+            tableName: 'MercadoPais',
             hooks: {
 
             }
-        });
+    });
+
 };
