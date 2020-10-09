4e7f0fc819666915d4b01ec249a8282c2fcfedf1
Yeni <hav_yeni@hotmail.com>
2020-10-09 08:47:09 -0400

    Actualizado modelo mercado
    Se eliminó el idmercado, el esquema y se actualizó el nombre de la tabla

--- api/modules/generated-module/models/mercado.js
+++ api/modules/generated-module/models/mercado.js
@@ -2,38 +2,43 @@
 var lodash = require('lodash');
 const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
-    const Mercado = global.app.orm.sequelize.define('Mercado',
-        lodash.extend({}, global.app.orm.mixins.attributes, {
-          "idmercado": {
-            "type": global.app.orm.Sequelize.INTEGER,
-            "allowNull": false,
-            "primaryKey": true
-          },
-            "activo": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "defaultValue": 1
-
-            },
-            "nombmercado": {
-                "type": global.app.orm.Sequelize.STRING
-
-            },
-            "codigo": {
-                "type": global.app.orm.Sequelize.STRING
-
-            },
-            "fultmodif": {
-                "type": global.app.orm.Sequelize.DATE,
-                "defaultValue": global.app.orm.Sequelize.NOW
-            },
-
-        }), {
+  const Mercado = global.app.orm.sequelize.define('Mercado',
+    lodash.extend({}, global.app.orm.mixins.attributes, {
+      "activo": {
+        "type": global.app.orm.Sequelize.INTEGER
+      },
+      "nombmercado": {
+        "type": global.app.orm.Sequelize.STRING
+      },
+      "codigo": {
+        "type": global.app.orm.Sequelize.STRING
+      },
+      "fultmodif": {
+        "type": global.app.orm.Sequelize.DATE
+      }
+    }), {
+      
             comment: 'A example model.',
+            timestamps: false,
+            paranoid: true,
             freezeTableName: true,
-            tableName: 'e_mercado',
-            schema: 'noconformidades',
+            tableName: 'Mercado',
             hooks: {
 
             }
-        });
+    });
+    Mercado.associate = function() {
+      var models = global.app.orm.sequelize.models;
+      models.Mercado.hasMany(models.ReservaPadre, {
+          foreignKey: 'idmercado',
+          constraints: false
+      });
+      models.Mercado.belongsToMany(models.Pais, {
+          through: models.MercadoPais
+      });
+      models.Mercado.belongsToMany(models.AgenciaViajes, {
+        through: models.AgenciaMercado
+      });
+    }
+
 };
