cb346cef8ca43f02c60e85074714519465f92ecf
Yeni <hav_yeni@hotmail.com>
2020-10-09 08:34:40 -0400

    Actualizado el modelo reservapadre

--- api/modules/generated-module/models/reservapadre.js
+++ api/modules/generated-module/models/reservapadre.js
@@ -4,17 +4,11 @@ const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
     const ReservaPadre = global.app.orm.sequelize.define('ReservaPadre',
         lodash.extend({}, global.app.orm.mixins.attributes, {
-          "idreservapadre": {
-            "type": global.app.orm.Sequelize.INTEGER,
-            "allowNull": false,
-            "primaryKey": true
-          },
             "fechacierre": {
                 "type": global.app.orm.Sequelize.DATE
             },
             "free": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "defaultValue": 0
+                "type": global.app.orm.Sequelize.INTEGER
 
             },
             "fechanotificacion": {
@@ -41,92 +35,78 @@ exports.loadModel = function loadModel() {
                 "type": global.app.orm.Sequelize.DATE
             },
             "esventaconticket": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "defaultValue": 0
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "idcliente": {
                 "type": global.app.orm.Sequelize.INTEGER
             },
             "idtipocliente": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
-            "MercadoID": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "references": {
-                    "model": "Mercado",
-                    "key": "idmercado"
-                },
-                "onUpdate": "cascade",
-                "onDelete": "cascade",
-                "allowNull": false
+            "idmercado": {
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "idrespuesta": {
-                "type": global.app.orm.Sequelize.STRING,
+                "type": global.app.orm.Sequelize.STRING
             },
             "idestado": {
-                "type": global.app.orm.Sequelize.STRING,
+                "type": global.app.orm.Sequelize.STRING
             },
             "referencia": {
-                "type": global.app.orm.Sequelize.STRING,
+                "type": global.app.orm.Sequelize.STRING
             },
             "cantmay": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "cantmen": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "cantinf": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_vend": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_usua_cierre": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_usua_creacion": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_usua_historico": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_usua_notificacion": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "idestadocupo": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "cantadm": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_usua_lock": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "iddepartamento": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "id_idio": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
             "observnotificacion": {
-                "type": global.app.orm.Sequelize.INTEGER,
+                "type": global.app.orm.Sequelize.INTEGER
             },
-            "PaisID": {
-                "type": global.app.orm.Sequelize.INTEGER,
-                "references": {
-                    "model": "Pais",
-                    "key": "idpais"
-                },
-                "onUpdate": "cascade",
-                "onDelete": "cascade",
-                "allowNull": false
+            "id_pais": {
+                "type": global.app.orm.Sequelize.INTEGER
             },
 
         }), {
             comment: 'A example model.',
+            timestamps: false,
+            paranoid: true,
             freezeTableName: true,
-            tableName: 'e_reservapadre',
-            schema: 'noconformidades',
+            tableName: 'ReservaPadre',
             hooks: {
 
             }
@@ -134,8 +114,26 @@ exports.loadModel = function loadModel() {
 
         ReservaPadre.associate = function() {
             var models = global.app.orm.sequelize.models;
-            models.ReservaPadre.belongsTo(models.Mercado);
-            models.ReservaPadre.belongsTo(models.Pais);
+            models.ReservaPadre.hasMany(models.Reserva, {
+                foreignKey: 'idreservapadre',
+                constraints: false
+            });
+            models.ReservaPadre.hasMany(models.QuejasReclamaciones, {
+                foreignKey: 'ReservaId',
+                constraints: false
+            });
+            models.ReservaPadre.hasMany(models.Turista, {
+                foreignKey: 'idreservapadre',
+                constraints: false
+            });
+            models.ReservaPadre.belongsTo(models.Mercado, {
+                foreignKey: 'idmercado',
+                constraints: false
+            });
+            models.ReservaPadre.belongsTo(models.Pais, {
+                foreignKey: 'id_pais',
+                constraints: false
+            });
         }
 
 };
