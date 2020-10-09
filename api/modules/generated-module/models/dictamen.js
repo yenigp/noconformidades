79aac7eea5347a3707e4c47501b6d771bef50dbb
Yeni <hav_yeni@hotmail.com>
2020-10-09 08:58:33 -0400

    Actualizado modelo dictamen
    Eliminado id, fecharecibido y esquema. Actualizado relación con Quejas y Reclamaciones, actualizado nombre de la tabla y los campos conclusiones, fecharecibido, aprobado, codigodictamen y quejasreclamacione_id

--- api/modules/generated-module/models/dictamen.js
+++ api/modules/generated-module/models/dictamen.js
@@ -4,13 +4,7 @@ const bcrypt = require('bcryptjs');
 exports.loadModel = function loadModel() {
     const Dictamen = global.app.orm.sequelize.define('Dictamen',
         lodash.extend({}, global.app.orm.mixins.attributes, {
-          "id": {
-            "type": global.app.orm.Sequelize.INTEGER,
-            "allowNull": false,
-            "autoIncrement": true,
-            "primaryKey": true
-          },
-          "quejasreclamacione_id": {
+          "QuejasReclamacionesId": {
               "type": global.app.orm.Sequelize.INTEGER,
               "references": {
                   "model": "QuejasReclamaciones",
@@ -20,22 +14,18 @@ exports.loadModel = function loadModel() {
               "onDelete": "cascade",
               "allowNull": false
           },
-            "codigodictamen": {
+            "codigo": {
                 "type": global.app.orm.Sequelize.STRING,
                 "allowNull": false
 
             },
-            "aprobado": {
-                "type": global.app.orm.Sequelize.BOOLEAN,
+            "estado": {
+                "type": global.app.orm.Sequelize.ENUM,
+                "values": ["aprobado", "denegado"],
                 "allowNull": true
 
             },
             "fechaaprobacion": {
-                "type": global.app.orm.Sequelize.DATEONLY,
-                "allowNull": false
-
-            },
-            "fecharecibido": {
                 "type": global.app.orm.Sequelize.DATEONLY,
                 "allowNull": false
 
@@ -45,19 +35,33 @@ exports.loadModel = function loadModel() {
                 "allowNull": false
 
             },
+            "CreatorId": {
+                "type": global.app.orm.Sequelize.INTEGER,
+                "references": {
+                    "model": "Usuario",
+                    "key": "id"
+                },
+                "onUpdate": "cascade",
+                "onDelete": "cascade"
+    
+            },
 
         }), {
             comment: 'A example model.',
             freezeTableName: true,
-            tableName: 'dictamen',
-            schema: 'noconformidades',
+            tableName: 'Dictamen',
             hooks: {
 
             }
         });
         Dictamen.associate = function() {
             var models = global.app.orm.sequelize.models;
-            models.Dictamen.belongsTo(models.QuejasReclamaciones);
+            models.Dictamen.belongsTo(models.Usuario, {
+                as: 'Creator'
+            });  
+            models.Dictamen.belongsTo(models.QuejasReclamaciones, {
+                as: 'QuejasReclamaciones'
+            });
         }
 
 };
